2169992eb5c55471ca609a09397061cc1bf5b7f3
nekomata <62155029+kyonES@users.noreply.github.com>
2021-11-12 11:14:44 +0900

    アルゴ式303

---
+++ 303.py
@@ -0,0 +1,9 @@
+# Q1-2. マスの移動 (1)
+# https://algo-method.com/tasks/303
+N = int(input())
+A = list(map(int, input().split()))
+dp = [0]*N
+dp[1] = A[1]
+for i in range(2, N):
+    dp[i] = min(dp[i-1]+A[i], dp[i-2]+A[i]*2)
+print(dp[-1])
